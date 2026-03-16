396fc3ca0543ba7bf80d65a36f2014df59f59a43
Sekhar2186 <somasekharkurapati6423@gmail.com>
2026-03-16 13:25:17 +0530

    done with codeInjector where it build codes and inject to the files

---
+++ lib/services/codeInjector.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+import path from "path";
+
+export async function codeInjector(projectPath: string, generatedCode: any) {
+    if (generatedCode.components) {
+        const compDir = path.join(projectPath, "components");
+        fs.mkdirSync(compDir, { recursive: true });
+        for (const fileName in generatedCode.components) {
+            const filePath = path.join(compDir, fileName);
+            fs.writeFileSync(filePath, generatedCode.components[fileName]);
+        }
+    }
+    if (generatedCode.routes) {
+        const apiDir = path.join(projectPath, "app", "api");
+
+        fs.mkdirSync(apiDir, { recursive: true });
+
+        for (const fileName in generatedCode.routes) {
+
+            const routeName = fileName.replace(".ts", "");
+
+            const routeFolder = path.join(apiDir, routeName);
+
+            fs.mkdirSync(routeFolder, { recursive: true });
+
+            const routeFile = path.join(routeFolder, "route.ts");
+
+            fs.writeFileSync(routeFile, generatedCode.routes[fileName]);
+        }
+    }
+    if (generatedCode.models) {
+
+        const modelDir = path.join(projectPath, "lib", "models");
+
+        fs.mkdirSync(modelDir, { recursive: true });
+
+        for (const fileName in generatedCode.models) {
+
+            const filePath = path.join(modelDir, fileName);
+
+            fs.writeFileSync(filePath, generatedCode.models[fileName]);
+        }
+    }
+}
